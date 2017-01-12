e037ac4487e84b12eea1ff90409b54e708d8c5cb
shadownater <shadownater@hotmail.com>
2017-01-11 18:03:44 -0700

    Updated to print itself

--- q2.py
+++ q2.py
@@ -4,7 +4,7 @@ import requests
 
 print requests.__version__
 
-response = requests.get("http://google.com/")
+response = requests.get("http://google.com/https://raw.githubusercontent.com/shadownater/cmput404lab1/master/q2.py")
 
 print response.text
 print response.status_code
